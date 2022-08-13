126385f03bebd3452d4fa215cc2ae9bff012b823
Zhoss <joss.90@mail.ru>
2022-08-13 15:03:01 +1000

    Выполненное ДЗ + задачи повышенной сложности по циклам и по переменным (№8)

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,12 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
+        task5();
+        task8();
     }
 
-    public static void task1() {
+    private static void task1() {
         int b = 0;
         while (b < 10) {
             b++;
@@ -20,13 +23,13 @@ public class Main {
         System.out.println();
     }
 
-    public static void task2() {
+    private static void task2() {
         for (int t = 3; t <= 31; t += 7) {
             System.out.println("Сегодня пятница, " + t + "-е число. Необходимо подготовить отчет!");
         }
     }
 
-    public static void task3() {
+    private static void task3() {
         LocalDate localDate = LocalDate.now();
         int b = localDate.getYear() - 200;
         int e = localDate.getYear() + 100;
@@ -36,4 +39,33 @@ public class Main {
             }
         }
     }
+
+    private static void task4() {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i + ": ping pong");
+                continue;
+            } else if (i % 3 == 0) {
+                System.out.println(i + ": ping");
+                continue;
+            } else if (i % 5 == 0) {
+                System.out.println(i + ": pong");
+                continue;
+            }
+            System.out.println(i + ":");
+        }
+    }
+
+    private static void task5() {
+        for (int a = 0, b = 1; a < 50; a = a + b, b = a + b) {
+            System.out.print(a + " " + b + " ");
+        }
+        System.out.println();
+    }
+    private static void task8() {
+        int a = 379;
+        int b = (a % 100) / 10;
+        System.out.println("Если a равно " + a + ", то b равно " + b);
+    }
+
 }
